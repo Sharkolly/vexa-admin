02f13b10151f2c1899d77463bc87563f5a8db082
Sharkolly <sharkollymofeoluwa@gmail.com>
2026-06-18 07:47:22 +0100

    feat: new change to the pages ui

--- api/api.ts
+++ api/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-  //  'http://localhost:5000/api/',
-    'https://vexa-server.onrender.com/api/',
+   'http://localhost:5000/api/',
+    // 'https://vexa-server.onrender.com/api/',
   withCredentials: true,
 });
 
--- src/App.tsx
+++ src/App.tsx
@@ -58,9 +58,9 @@ function App() {
           <Route
             path="/product-form"
             element={
-              <ProtectedRoute>
+              // <ProtectedRoute>
                 <ProductForm />
-              </ProtectedRoute>
+              // </ProtectedRoute>
             }
           />
 
